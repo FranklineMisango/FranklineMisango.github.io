bfc824718b4a9c56f1128caa4acbf47139d7efe7
FranklineMisango <franklinemisango@gmail.com>
2023-01-16 18:43:11 +0800

    Changes to the PHP to gmail

--- mail.php
+++ mail.php
@@ -1,53 +1,43 @@
 <?php
 
-    // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $phone = trim($_POST["phone"]);
-        $message = trim($_POST["message"]);
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-        // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($phone) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Please complete the form and try again.";
-            exit;
-        }
+require 'phpmailer/src/Exception.php'
+require 'phpmailer/src/PHPMailer.php'
+require 'phpmailer/src/SMTP.php'
 
-        // Set the recipient email address.
+if (isset($_POST["send"])){
+    $mail = new PHPMailer(true);
+    $mail -> isSMTP();
+    $mail -> Host = 'smtp.gmail.com';
+    $mail -> SMTPAuth = true;
+    $mail -> Username = 'franklinemisango4@gmail.com';
+    $mail ->Password = "frankmisango"
+    $mail ->SMTPSecure = 'ssl';
+    $mail -> Port = 465;
+    $mail -> setFrom('franklinemisango4@gmail.com');
+    $mail -> <addAddress($_POST["email"]);
+    $mail ->Message = $_POST["Name"];
+    $mail ->Message = $_POST["Email"];
+    $mail ->Message = $_POST["Phone"];
+    $mail ->Message = $_POST["Subject"];
+    $mail ->Message = $_POST["Message"];
+    $mail ->send();
 
-        $recipient = "fmo2704@connect.hku.hk";
+    echo 
+    "
+    <script>
+    alert('Your Message has been sent successfully');
+    document.location.href = index.html;
+    </script>
+    
+    "
 
-        // Set the email subject.
-        $subject = "New contact from $name";
 
-        // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Phone: $phone\n\n";
-        $email_content .= "Message:\n$message\n";
 
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
 
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
-        }
 
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
-    }
 
+}
 ?>
